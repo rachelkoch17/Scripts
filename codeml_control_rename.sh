120e0f0f098c71b61e55a12f89626b892f38d155
Rachel Koch <48254439+rachelkoch17@users.noreply.github.com>
2020-04-10 17:10:14 -0500

    Update codeml_control_rename.sh

--- codeml_control_rename.sh
+++ codeml_control_rename.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+#usage: ./codeml_control_rename.sh codeml_control_file.ctl list_of_hogs.txt
+
 control_file=$1
-for x in $(cat hogs_dogs.txt); do
+hogs_list=$2
+for x in $(cat $2); do
 cat $1 | sed "s/seqfile =/seqfile = $x.phy/"  | \
 sed "s/treefile =/treefile = $x.tree/"  | \
-sed "s/outfile =/outfile = $x.out/" > $x.good.ctl;
+sed "s/outfile =/outfile = $x.out/" > $x.ctl;
 done;
