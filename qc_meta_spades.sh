bbbe6fcc92f790672818c3374937ef4194da93dd
Rachel Koch <48254439+rachelkoch17@users.noreply.github.com>
2020-05-02 15:58:36 -0500

    Update qc_meta_spades.sh

--- qc_meta_spades.sh
+++ qc_meta_spades.sh
@@ -14,7 +14,7 @@ spades.py --meta \
 --pe1-1 ${filename1}_R1_um_clean.fq \
 --pe1-2 ${filename1}_R2_um_clean.fq \
 --pe1-s ${filename1}_um_clean_singles.fq \
--m 500 \
+-m 62 \
 -t 72 \
 -k 31,41,51,61,71,81,91,101,111,121 \
 -o ${filename1}_meta_spades_assembly
